2387b5a02c3f3ab538d8577acbba66cdba90256c
Sunny <kolatts@gmail.com>
2016-06-01 08:39:49 -0400

    Added azure mobile services

--- Droid/LinkerDummyClass.cs
+++ Droid/LinkerDummyClass.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using Splat;
-using StockVizForms.Model;
+﻿using Splat;
+using System;
 
 namespace StockVizForms.Droid
 {
     public static class ShutUpLinkerIDoWhatIWant
     {
-        public static string StopDeletingMyStuff ()
+        public static string StopDeletingMyStuff()
         {
-            return typeof (WrappingFullLogger).ToString ();
+            return typeof(WrappingFullLogger).ToString();
         }
 
-        public static Type Blah = typeof (Akavache.BlobCache);
+        public static Type Blah = typeof(Akavache.BlobCache);
     }
 }
-
---
+++ StockVizForms.Azure/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StockVizForms.Azure.WebApiApplication" Language="C#" %>
